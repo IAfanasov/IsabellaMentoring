71dc810a4181487dd78cbd1beb4f31d6f6fe3b3c
Isabella Dang <isabelladang173@gmail.com>
2020-08-05 17:14:27 +0200

    Update getTheFirstNonRepeatedCharacter.js

--- tasks/functions/23. first non repeative/getTheFirstNonRepeatedCharacter.js
+++ tasks/functions/23. first non repeative/getTheFirstNonRepeatedCharacter.js
@@ -3,7 +3,20 @@
  * @returns {string}
  */
 function getTheFirstNonRepeatedCharacter(text) {
-    
-    return null;
+    const frequencyCounter = {}
+    for (let val of text) {
+        frequencyCounter[val] = (frequencyCounter[val] || 0) + 1;
+    }
+    console.log(frequencyCounter);
+    // Convert the object into array
+    const countedArray = Object.entries(frequencyCounter);
+    console.log(countedArray);
+    // Find the array element = 1
+    let found = countedArray.find(element => element[1] === 1);
+    if (found === undefined) {
+        return null
+    } else {
+        return found[0];
+    }
 }
 module.exports = getTheFirstNonRepeatedCharacter;
